ec23050e51558d5bbfc8e4baf46b4f612d3ae429
ManausK <1905246@kiit.ac.in>
2023-05-11 16:20:54 +0530

    cart css and buttons added

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import { PRODUCTS } from "../../products";
+import { ShopContext } from "../../context/ShopContext";
+import { CartItem } from "./cartItem";
+import "./cart.css";
 
 export const Cart = () => {
-  return <div>cart</div>;
+  const { addToCart, cartItems } = useContext(ShopContext);
+
+  return (
+    <div className="cart">
+      <div>
+        <h1>Your Cart Items</h1>
+      </div>
+      <div className="cartItems">
+        {PRODUCTS.map((product) => {
+          if (cartItems[product.id] !== 0) {
+            return <CartItem data={product} />;
+          }
+        })}
+      </div>
+    </div>
+  );
 };
